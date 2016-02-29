01954414815a41afc522338af816e3b22bd9276e
sebastian correa morales <correa.m.sebastian@gmail.com>
2016-02-29 09:37:52 -0500

    init commit, funcionando ok,  voy a añadir un template mas elegante

---
+++ readme.md
@@ -0,0 +1,6 @@
+#build with package 
+
+i only have to do 
+ionic io init
+
+and later ionic package  --optiosn
---
+++ www/js/directives.js
@@ -0,0 +1,66 @@
+var vmD;
+(function() {
+    'use strict';
+
+    angular
+        .module('starter.directives', ['ngCordova'])
+        .directive('checkNetwork', checkNetwork);
+
+    checkNetwork.$inject = [];
+
+    /* @ngInject */
+    function checkNetwork() {
+        // Usage:
+        //
+        // Creates:
+        //
+        var directive = {
+            bindToController: true,
+            controller: Controller,
+            controllerAs: 'vm',
+            link: link,
+            restrict: 'EA',
+            scope: {},
+            template: '<span>{{vm.state}}</span>'
+        };
+        return directive;
+
+        function link(scope, element, attrs) {}
+    }
+
+    Controller.$inject = ['$rootScope', '$cordovaNetwork', '$ionicPlatform']
+
+    /* @ngInject */
+    function Controller($rootScope, $cordovaNetwork, $ionicPlatform) {
+        var vm = this;
+        vmD = vm;
+
+        vm.state = 'online inicial true por default';
+
+        $ionicPlatform.ready()
+            .then(activate)
+
+        function activate() {
+
+            function innerCheck(msg) {
+                vm.state = msg + $cordovaNetwork.isOnline();
+                console.log(vm.state);
+            }
+
+
+            innerCheck('on activate is')
+
+            // listen for Online event
+            $rootScope.$on('$cordovaNetwork:online', function(event, networkState) {
+                innerCheck('$cordovaNetwork:online')
+            })
+
+            // listen for Offline event
+            $rootScope.$on('$cordovaNetwork:offline', function(event, networkState) {
+                innerCheck('$cordovaNetwork:offline')
+            })
+
+        }
+
+    }
+})();
